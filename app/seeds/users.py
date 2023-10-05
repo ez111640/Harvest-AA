692e1da62361e6704a6dae5147704aa89048f490
Erica Zimmerman <ericanzimmerman@gmail.com>
2023-10-05 12:12:04 -0500

    commit

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,7 +5,7 @@ from ..models.db import SCHEMA, environment
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password', firstName="Demo", lastName="Zimmerman", city="Green Bay", state="WI")
+        username='Demo', email='demo@aa.io', password='password', firstName="Emma", lastName="Zimmerman", city="Green Bay", state="WI")
     demo2 = User(
         username='Demo2', email='demo2@aa.io', password='password', firstName="Kayla", lastName="Hatle", city="Sioux City", state="SD")
     demo3 = User(
